8e4872ec2990c78581742872703a666a1d4d6ce7
cxcollins <collicon@oregonstate.edu>
2024-11-21 01:15:11 -0800

    Southwest component much more visually appealing now

--- src/Southwest.js
+++ src/Southwest.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import DatePicker from 'react-datepicker'
 import './Southwest.css'
 
 export default function Southwest() {
@@ -85,55 +86,82 @@ export default function Southwest() {
         <div className='Page-container'>
             <h1 className='Southwest-h1'>Southwest Price Change Alert</h1>
             <p>Enter your flight info to be alerted if your flight drops below what you paid for it. Since Southwest will allow you to rebook and pocket the difference, it is worth checking everyday for a change in price.</p>
-            <p>This script runs twice a day (at noon and midnight), and will send an email alert if there is a change. Note that only 'Wanna get away' tickets are supported.</p>
+            <p>This script runs daily at noon, and will send an email alert if there is a change. Note that only 'Wanna get away' tickets are supported.</p>
             <div className='Southwest-container' id='Southwest-container'>
-                {/* Add on submit function */}
-                <form id='form' onSubmit={(e) => { e.preventDefault()}}>
+                <form id='SwForm' onSubmit={(e) => { e.preventDefault()}}>
+                    <div className='formUnit'>
                     <label htmlFor='pricePaid'>How much did you pay for your flight? </label>
-                    <input id='pricePaid' type='number' value={pricePaid} onChange={handleChange} required></input>
+                    <input className='SwInput' id='pricePaid' type='number' value={pricePaid} onChange={handleChange} required></input>
+                    </div>
                     <fieldset id='departureDetails'>
                         <legend>
                             Departure details
                         </legend>
+                        <div className='formUnit'>
                         <label htmlFor='departureDate'>Departure date:</label>
-                        <input id='departureDate' placeholder='1/1/24' type='date' value={departureDate} onChange={handleChange} required></input>
+                        {/* <DatePicker id='departureDate' selected={departureDate} onChange={handleChange} className='SwInput' required /> */}
+                        <input className='SwInput' id='departureDate' type='date' value={departureDate} onChange={handleChange} required></input>
+                        </div>
+                        <div className='formUnit'>
                         <label htmlFor='departureTime'>Departure time: </label>
-                        <input id='departureTime' placeholder='HH:MM' type='text' value={departureTime} onChange={handleChange} required></input>
+                        <input className='SwInput' id='departureTime' placeholder='HH:MM' type='text' value={departureTime} onChange={handleChange} required></input>
+                        </div>
+                        <div className='formUnit'>
                         <label htmlFor='departureMeridiem'>AM/PM: </label>
-                        <input id='departureMeridiem' placeholder='AM or PM' type='text' value={departureMeridiem} onChange={handleChange} required></input>
-                        <label htmlFor='arrivalTime'>Arrival time: </label>
-                        <input id='arrivalTime' placeholder='HH:MM' type='text' value={arrivalTime} onChange={handleChange} required></input>
-                        <label htmlFor='arrivalMeridiem'>AM/PM: </label>
-                        <input id='arrivalMeridiem' placeholder='AM or PM' type='text' value={arrivalMeridiem} onChange={handleChange} required></input>
-                        <label htmlFor='departureAirport'>Departure airport code: </label>
-                        <input id='departureAirport' placeholder='SFO' type='text' value={departureAirport} onChange={handleChange} required></input>
-                        <label htmlFor='arrivalAirport'>Arrival airport code: </label>
-                        <input id='arrivalAirport' placeholder='BOS' type='text' value={arrivalAirport} onChange={handleChange} required></input>
+                        <input className='SwInput' id='departureMeridiem' placeholder='AM or PM' type='text' value={departureMeridiem} onChange={handleChange} required></input>
+                        </div>
+                        <div className='formUnit'>
+                            <label htmlFor='arrivalTime'>Arrival time: </label>
+                            <input className='SwInput' id='arrivalTime' placeholder='HH:MM' type='text' value={arrivalTime} onChange={handleChange} required></input>
+                        </div>
+                        <div className='formUnit'>
+                            <label htmlFor='arrivalMeridiem'>AM/PM: </label>
+                            <input className='SwInput' id='arrivalMeridiem' placeholder='AM or PM' type='text' value={arrivalMeridiem} onChange={handleChange} required></input>
+                        </div>
+                        <div className='formUnit'>
+                            <label htmlFor='departureAirport'>Departure airport code: </label>
+                            <input className='SwInput' id='departureAirport' placeholder='SFO' type='text' value={departureAirport} onChange={handleChange} required></input>
+                        </div>
+                        <div className='formUnit'>
+                            <label htmlFor='arrivalAirport'>Arrival airport code: </label>
+                            <input className='SwInput' id='arrivalAirport' placeholder='BOS' type='text' value={arrivalAirport} onChange={handleChange} required></input>
+                            </div>
                         <label htmlFor='roundtrip'>Roundtrip
-                            <input type='radio' id='roundtrip' name='roundtrip' value='roundtrip' checked={roundtrip === 'roundtrip'} onChange={handleRadioChange} />
+                            <input className='SwRadio' type='radio' id='roundtrip' name='roundtrip' value='roundtrip' checked={roundtrip === 'roundtrip'} onChange={handleRadioChange} />
                         </label>
                         <label htmlFor='oneway'>Oneway
-                            <input type='radio' id='oneway' name='roundtrip' value='oneway' checked={roundtrip === 'oneway'} onChange={handleRadioChange}/>
+                            <input className='SwRadio' type='radio' id='oneway' name='roundtrip' value='oneway' checked={roundtrip === 'oneway'} onChange={handleRadioChange}/>
                         </label>
                     </fieldset>
 
                     {roundtrip === 'roundtrip' && (<fieldset id='roundtripDetails'>
                         <legend>Return flight details</legend>
-                        <label htmlFor='returnDate'>Departure date:</label>
-                        <input id='returnDate' placeholder='1/8/24' type='date' value={returnDate} onChange={handleChange} required></input>
-                        <label htmlFor='returnDepartureTime'>Departure time: </label>
-                        <input id='returnDepartureTime' placeholder='HH:MM' type='text' value={returnDepartureTime} onChange={handleChange} required></input>
-                        <label htmlFor='returnDepartureMeridiem'>AM/PM: </label>
-                        <input id='returnDepartureMeridiem' placeholder='AM or PM' type='text' value={returnDepartureMeridiem} onChange={handleChange} required></input>
-                        <label htmlFor='returnArrivalTime'>Arrival time: </label>
-                        <input id='returnArrivalTime' placeholder='HH:MM' type='text' value={returnArrivalTime} onChange={handleChange} required></input>
-                        <label htmlFor='returnArrivalMeridiem'>AM/PM: </label>
-                        <input id='returnArrivalMeridiem' placeholder='HH:MM' type='text' value={returnArrivalMeridiem} onChange={handleChange} required></input>
+                        <div className='formUnit'>
+                            <label htmlFor='returnDate'>Departure date:</label>
+                            <input className='SwInput' id='returnDate' type='date' value={returnDate} onChange={handleChange} required></input>
+                        </div>
+                        <div className='formUnit'>
+                            <label htmlFor='returnDepartureTime'>Departure time: </label>
+                            <input className='SwInput' id='returnDepartureTime' placeholder='HH:MM' type='text' value={returnDepartureTime} onChange={handleChange} required></input>
+                        </div>
+                        <div className='formUnit'>
+                            <label htmlFor='returnDepartureMeridiem'>AM/PM: </label>
+                            <input className='SwInput' id='returnDepartureMeridiem' placeholder='AM or PM' type='text' value={returnDepartureMeridiem} onChange={handleChange} required></input>
+                        </div>
+                        <div className='formUnit'>
+                            <label htmlFor='returnArrivalTime'>Arrival time: </label>
+                            <input className='SwInput' id='returnArrivalTime' placeholder='HH:MM' type='text' value={returnArrivalTime} onChange={handleChange} required></input>
+                        </div>
+                        <div className='formUnit'>
+                            <label htmlFor='returnArrivalMeridiem'>AM/PM: </label>
+                            <input className='SwInput' id='returnArrivalMeridiem' placeholder='HH:MM' type='text' value={returnArrivalMeridiem} onChange={handleChange} required></input>
+                        </div>
                     </fieldset>)}
-
-                    <label htmlFor='email'>What email would you like to be contacted at? </label>
-                    <input id='email' placeholder='username@example.com' type='email' value={email} onChange={handleChange} className='spaced' required></input>
-                    <button type="submit" onClick={addFlight}>Submit</button>
+                    <div className='formUnit'>
+                        <label htmlFor='email'>What email would you like to be contacted at? </label>
+                        <input className='SwInput' id='email' placeholder='username@example.com' type='email' value={email} onChange={handleChange} required></input>
+                    <button type="submit" onClick={addFlight} className='swButton'>Submit</button>
+                    </div>
                     </form>
             </div>
         </div>
